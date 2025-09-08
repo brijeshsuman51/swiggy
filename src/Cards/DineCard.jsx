225af9206a2195b68ea747d6b741b0d6b7679f41
Brijesh Suman <brijeshsuman461@gmail.com>
2025-09-08 13:26:36 +0530

    second commit

--- src/Cards/DineCard.jsx
+++ src/Cards/DineCard.jsx
@@ -2,9 +2,18 @@
 
 export default function DineCard({dineData}) {
     return (
-        <div>
+        <div className="max-w-sm flex-none">
         <a target="_blank" href={dineData?.cta?.link}>
-            <img className="w-50 h-40" src={'https://media-assets.swiggy.com/swiggy/image/upload/'+dineData?.info?.mediaFiles[0]?.url} alt="Restaurant"/>
+            <div className="relative">
+            <img className="w-50 h-40 object-cover" src={'https://media-assets.swiggy.com/swiggy/image/upload/'+dineData?.info?.mediaFiles[0]?.url} alt="Restaurant"/>
+            <div className="absolute bottom-0 left-0 right-0 h-16 bg-gradient-to-t from-black to-transparent"></div>
+            <p className="absolute bottom-2 left-2 text-xl text-white z-10">
+                {dineData?.info?.name}
+            </p>
+            <p className="absolute bottom-2 right-2 text-xl text-white z-10">
+                {dineData?.info?.rating?.value}
+            </p>
+        </div>
         </a>
         </div>
     )
--- src/Components/DineOption.jsx
+++ src/Components/DineOption.jsx
@@ -4,6 +4,11 @@ import { DineData } from "../utils/DineData";
 
 export default function DineOption(){
     return(
-        DineData.map((dineData)=><DineCard key={DineData?.info?.id} dineData={dineData}></DineCard>)
+        <div className="w-[80%] mx-auto mt-20 mb-20">
+            <p className="text-3xl font-bold">Discover best Restaurants for Dineout</p>
+            <div className="flex flex-nowrap overflow-x-auto mt-5 gap-4">
+       { DineData.map((dineData)=><DineCard key={DineData?.info?.id} dineData={dineData}></DineCard>)}
+        </div>
+        </div>
     )
 }
